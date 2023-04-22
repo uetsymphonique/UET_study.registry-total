0409e6b3e35f759d067c543e527e9e300d908780
uetsymphonique <minhtran6464.lhp@gmail.com>
2023-04-22 07:45:00 +0700

    nodemailer and config

---
+++ utils/email.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async options => {
+    // 1) Create a transporter
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    // 2) Define the email options
+    const mailOptions = {
+        from: options.sender, // sender address
+        to: options.receiver, // list of receivers
+        subject: options.subject, // Subject line
+        text: options.text // plain text body
+        //html:
+    };
+    // 3) Actually send the email
+    transporter.sendMail(mailOptions, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Email sent:'+ result.message);
+        }
+    });
+}
+
+module.exports = sendEmail;
