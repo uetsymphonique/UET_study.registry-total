594d0429847b7a7ddb40d2ee4663db40250e699b
uetsymphonique <minhtran6464.lhp@gmail.com>
2023-05-21 17:10:27 +0700

    Change expire of jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,13 +7,13 @@ const crypto = require('crypto');
 const {promisify} = require('util')
 const signToken = id => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
+        expiresIn: process.env.JWT_EXPIRES_IN * 60 * 60 * 1000
     })
 };
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
     const cookieOptions = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000),
         httpOnly: true
     }
     if (process.env.NODE_ENV === 'production') {
