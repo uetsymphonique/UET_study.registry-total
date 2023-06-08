c11088d87c7dfb6458fe3089c05d4cd5f3d711e9
uetsymphonique <minhtran6464.lhp@gmail.com>
2023-06-08 15:28:56 +0700

    Commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(helmet());
 app.use(cors());
 
 //development logging
-if (process.env.NODE_ENV.toString().trim() === "development") {
+if (process.env.NODE_ENV.trim() === "development") {
   app.use(logger("dev"));
 }
 
