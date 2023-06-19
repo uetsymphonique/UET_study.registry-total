d908dd701a581a46366d6a7825133bf64ca7f628
uetsymphonique <minhtran6464.lhp@gmail.com>
2023-06-20 00:26:25 +0700

    Remove upload car to deploy in vercel

--- controllers/carController.js
+++ controllers/carController.js
@@ -55,18 +55,18 @@ exports.inspectCar = catchAsync(async (req, res, next) => {
             }
         })
 });
-exports.upload = catchAsync(async (req, res, next) => {
-    if (!req.file || !req.file.filename) {
-        return next(new AppError('Không có file nào được tải lên!', 400));
-    } else {
-        const data = await Car.create(xlsxToJson(req.file.path));
-        res.status(201)
-            .json({
-                status: 'success',
-                // data
-            })
-    }
-});
+// exports.upload = catchAsync(async (req, res, next) => {
+//     if (!req.file || !req.file.filename) {
+//         return next(new AppError('Không có file nào được tải lên!', 400));
+//     } else {
+//         const data = await Car.create(xlsxToJson(req.file.path));
+//         res.status(201)
+//             .json({
+//                 status: 'success',
+//                 // data
+//             })
+//     }
+// });
 exports.currentMonthExpiredPredictionsOfAllCentres = catchAsync(async (req, res, next) => {
     const features = new APIFeatures_aggregate(Inspection.aggregate([
         ...pipeline_groupCars,
